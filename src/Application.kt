2bc8a358eed5b8118745ff0609a748692c35352d
grigoriy <grisha.56@icloud.com>
2020-02-24 22:38:41 +0300

    Loop work

--- src/Application.kt
+++ src/Application.kt
@@ -14,85 +14,82 @@ fun main(args: Array<String>) {
     var size: Int = 0
 
     println("Данная программа позволяет решить СЛАУ методом Гаусса-Зейделя")
-    loop@ while (true) {
-        println(DIVIDER)
-        println("Главное меню:")
-        println("1) Ввод коэффициентов с клавиатуры")
-        println("2) Ввод коэффициентов из файла")
-        println("0) Выход")
-        print("Ввод: ")
-        try {
-            when (scanner.next().toInt()) {
-                1 -> {
-                    println(DIVIDER)
-                    println("Вы выбрали ввод с консоли")
-                    inputReader.consoleCoefficientReader(scanner)
-                    coefficients = inputReader.coefficients
-                    eps = inputReader.eps
-                    size = inputReader.size
-                    break@loop
-                }
-                2 -> {
-                    println("Вы выбрали ввод из файла")
-                    while (true) {
+    while (true) {
+        loop@ while (true) {
+            println(DIVIDER)
+            println("Главное меню:")
+            println("1) Ввод коэффициентов с клавиатуры")
+            println("2) Ввод коэффициентов из файла")
+            println("0) Выход")
+            print("Ввод: ")
+            try {
+                when (scanner.next().toInt()) {
+                    1 -> {
                         println(DIVIDER)
-                        println("Формат файла")
-                        println("Первая строчка - точность (eps)")
-                        println("Вторая строчка - размер матрицы (n)")
-                        println("Следующие n строк - n элементов матрицы системы")
-                        println("Последняя строка - столбец свободных членов")
-                        print("Введите имя файла: ")
-                        try {
-                            val fileReader = Scanner(File(scanner.next()))
-                            inputReader.fileCoefficientReader(fileReader)
-                            fileReader.close()
-                            coefficients = inputReader.coefficients
-                            eps = inputReader.eps
-                            size = inputReader.size
-                            break@loop
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            println("Возникла ошибка при вводе из файла. Проверьте имя файла и его формат.")
+                        println("Вы выбрали ввод с консоли")
+                        inputReader.consoleCoefficientReader(scanner)
+                        coefficients = inputReader.coefficients
+                        eps = inputReader.eps
+                        size = inputReader.size
+                        break@loop
+                    }
+                    2 -> {
+                        println("Вы выбрали ввод из файла")
+                        while (true) {
                             println(DIVIDER)
+                            println("Формат файла")
+                            println("Первая строчка - точность (eps)")
+                            println("Вторая строчка - размер матрицы (n)")
+                            println("Следующие n строк - n элементов матрицы системы")
+                            println("Последняя строка - столбец свободных членов")
+                            print("Введите имя файла: ")
+                            try {
+                                val fileReader = Scanner(File(scanner.next()))
+                                inputReader.fileCoefficientReader(fileReader)
+                                fileReader.close()
+                                coefficients = inputReader.coefficients
+                                eps = inputReader.eps
+                                size = inputReader.size
+                                break@loop
+                            } catch (e: Exception) {
+                                println("Возникла ошибка при вводе из файла. Проверьте имя файла и его формат.")
+                            }
                         }
                     }
+                    0 -> {
+                        println("Выход из программы...")
+                        exitProcess(0)
+                    }
+                    else -> throw Exception()
                 }
-                0 -> {
-                    println("Выход из программы...")
-                    exitProcess(0)
+            } catch (e: Exception) {
+                println("Вы ввели неверное число. Попробуйте еще раз.")
+            }
+        }
+
+        val solver = GaussSeidel(eps, coefficients!!, size)
+        val solution = solver.solve()
+
+        println(DIVIDER)
+        if (solution != null) {
+            println("Заданная точность: $eps")
+            println("Заданное количество элементов: $size")
+            println("Матрица: ")
+            for (i in 0 until size) {
+                for (j in 0 until size) {
+                    print("${coefficients[i][j]} ")
                 }
-                else -> throw Exception()
+                println("| ${coefficients[i][size]}")
             }
-        } catch (e: Exception) {
-            println("Вы ввели неверное число. Попробуйте еще раз.")
-            println(DIVIDER)
-        }
-    }
 
-    println("РЕШАЮ...")
-
-    val solver = GaussSeidel(eps, coefficients!!, size)
-    val solution = solver.solve()
-
-    println(DIVIDER)
-    if (solution != null) {
-        println("Заданная точность: $eps")
-        println("Заданное количество элементов: $size")
-        println("Матрица: ")
-        for (i in 0 until size) {
-            for (j in 0 until size) {
-                print("${coefficients[i][j]} ")
+            println("Решение системы:")
+            for (i in solution.indices) {
+                println("x[${i + 1}]: ${solution[i]}")
             }
-            println("| ${coefficients[i][size]}")
+            println("Количество итераций: ${solver.counter}")
+        } else {
+            println("Невозможно решить систему.\nНевозможно привести матрицу к виду диагонального преобладания.")
         }
-
-        println("Решение системы:")
-        for (i in solution.indices) {
-            println("x[${i + 1}]: ${solution[i]}")
-        }
-        println("Количество итераций: ${solver.counter}")
-    } else {
-        println("Невозможно решить систему.\nНевозможно привести матрицу к виду диагонального преобладания.")
     }
 
 }
